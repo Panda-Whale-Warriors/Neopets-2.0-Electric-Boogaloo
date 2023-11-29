fb4d707119f5e68c9cb1e76c575deaa51137df1f
Donald-Macak <don.mack83@ymail.com>
2023-11-29 15:30:21 -0600

    Authentication+Sessions

---
+++ server/controller/sessionController.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const Session = require("../model/sessionModel");
+const sessionController = {};
+
+/**
+ * isLoggedIn - find the appropriate session for this request in the database, then
+ * verify whether or not the session is still valid.
+ */
+sessionController.isLoggedIn = async (req, res, next) => {
+  // write code here
+  // console.log('are we here');
+  // console.log('req.cookies: ', req.cookies);
+  try {
+    // console.log("are we there");
+    const cookieId = req.cookies.ssid || null;
+    // console.log("our cookie id: ", cookieId);
+    const session = await Session.findOne({ cookieId: cookieId });
+    // console.log("this is seesion maybe: ", session);
+    if (session) {
+      console.log("are we here: ");
+      // will need to res.redirect to whatever 'home' page is for the user
+      res.redirect("/secret"); // leftover from auth unit, we won't actually be redirecting to secret.
+    }
+
+    console.log("are we not here");
+    next();
+  } catch (err) {
+    next("Error in sessionController.isLoggedIn: " + JSON.stringify(err));
+  }
+};
+
+/**
+ * startSession - create and save a new Session into the database.
+ */
+sessionController.startSession = async (req, res, next) => {
+  //write code here
+  try {
+    console.log("locating cookie....");
+    // its only on request initially
+    // const ssid = req.cookies.ssid;
+    const ssid = res.locals.ssid;
+    console.log("ssid", ssid);
+    await Session.create({ cookieId: ssid });
+    console.log("session created");
+    return next();
+  } catch (err) {
+    return next(
+      "Error in sessionController.startSession: " + JSON.stringify(err)
+    );
+  }
+};
+
+sessionController.logOut = async (req, res, next) => {
+  try {
+    console.log("attempting to logout...");
+    const ssid = req.cookies.ssid;
+    // console.log("ssid", ssid);
+    await Session.findOneAndDelete({ cookieId: ssid });
+    console.log("session cleared from database");
+    // some way to delete ssid cookie from browser session?
+    res.clearCookie("ssid");
+    console.log("ssid cookie cleared from browser");
+    // console.log("req.cookies", req.cookies);
+    // res.redirect("/login");
+    return next();
+  } catch (err) {
+    return next({
+      log: "Express error handler caught error in sessionController.logOut middleware",
+      status: 300,
+      message: { err: "Encountered error in logout process" },
+    });
+  }
+};
+// const sessionSchema = new Schema({
+//   cookieId: { type: String, required: true, unique: true },
+//   createdAt: { type: Date, expires: 30, default: Date.now }
+// });
+module.exports = sessionController;
--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,4 +1,4 @@
-const User = require('../model/userModel');
+const User = require("../model/userModel");
 
 const userController = {};
 
@@ -12,47 +12,74 @@ userController.getAllUsers = (req, res, next) => {
       return next();
     })
     .catch((error) => {
-      console.log('Error in getting all users', error);
-      return res.status(500).json({ error: 'Internal Server Error' });
+      console.log("Error in getting all users", error);
+      return res.status(500).json({ error: "Internal Server Error" });
     });
 };
 
 //  createUser - create and save a new User into the database.
 userController.createUser = (req, res, next) => {
   const { username, password } = req.body;
+  console.log("username: ", username);
+  console.log("password: ", password);
   User.create({ username, password })
     .then((user) => {
-      res.locals.createUser = user;
+      res.locals.createUser = user.username;
       next();
     })
     .catch((error) => {
-      console.log('Error in creating a user', error);
-      return res.status(500).json({ error: 'Internal Server Error' });
+      console.log("Error in creating a user", error);
+      return res.status(500).json({ error: "Internal Server Error" });
     });
 };
 
 // verifyUser - Obtain username and password from the request body, locate
 //  * the appropriate user in the database, and then authenticate the submitted password
 //  * against the password stored in the database.
-userController.verifyUser = (req, res, next) => {
-  console.log('req.body', req.body);
-  console.log('req.params', req.params);
-  console.log('req.query', req.query);
-  console.log('req', req);
-  User.findOne({ username: req.body.username }).then((user) => {
-    // check if user is present or if the user's password is the same as
+userController.verifyUser = async (req, res, next) => {
+  // deconstruct username and password from req.body
+  const { username, password } = req.body;
+
+  try {
+    // attempt to find user in database by username
+    const user = await User.findOne({ username: username });
+    console.log("here is the user from DB", user);
+    // if no users are found in database, redirect back to signup
+    if (!user) {
+      console.log("No user found by that name");
+      return res.redirect("/signup");
+    }
+    console.log("about to run bcrypt password compare");
+    // using bcrypt comparePassword method to check if passwords match
+    const passwordMatch = await user.comparePassword(password);
+    console.log("password compare completed....");
+    // if passwords DO NOT match, redirect user to the login page to try again
+    if (!passwordMatch) {
+      console.log("User Verification failed");
+      return res.redirect("/login");
+    }
+
+    // if no other conditionals were hit, return next() to move on to next middleware.
+    return next();
+  } catch (err) {
+    next({
+      log: "Express error handler caught error in userController middleware",
+      status: 500,
+      message: { err: "Unable to verify user data" },
+    });
+  }
+};
+module.exports = userController;
+
+/*  // check if user is present or if the user's password is the same as
     // req.body's password, then
     // else, redirect to sign up page
-    if (!user || user.password !== req.body.password) {
-      console.log('this is body', req.body);
-      res.redirect('/signup');
+    if (!user || user.password !== password) {
+      console.log("this is body", req.body);
+      return res.redirect("/signup");
     } else {
       res.locals._id = user._id;
       res.locals.username = user.username;
       res.locals.password = user.password;
-      console.log('made it to line 66 of verify user!');
-      next();
-    }
-  });
-};
-module.exports = userController;
+      console.log("you have successfully passed login credentials in");
+      next();*/
